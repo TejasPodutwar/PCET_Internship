120ef0e43ffdaf43d4258e07f92489ff55d22a0e
Tejas <tejaskumar8501@gmail.com>
2021-10-28 13:58:53 +0530

    Update Testimonial

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,5 +1,22 @@
+$('.testi.owl-carousel').owlCarousel({
+    loop:true,
+    dots:false,
+    items: 2,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout: 3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        768:{
+            items:2
+        }
+    }
 
-$('.owl-carousel').owlCarousel({
+  });
+
+$('.default1.owl-carousel').owlCarousel({
     loop:true,
     dots:false,
     autoplay:true,
